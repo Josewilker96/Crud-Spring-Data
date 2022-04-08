ca217d03ee73499e1975530d00a2379da2ea7e0d
Josewilker96 <zwilker@icloud.com>
2022-04-07 21:04:34 -0300

    Crud Spring-data
    Crud com mongoDB e mapeamentos utilizando spring-data-jpa

---
+++ Spring-data-jpa/src/main/java/br/com/alura/spring/data/specification/SpecificationFuncionario.java
@@ -0,0 +1,33 @@
+package br.com.alura.spring.data.specification;
+
+
+import java.time.LocalDate;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import br.com.alura.spring.data.orm.Funcionario;
+
+public class SpecificationFuncionario {
+
+	public static Specification<Funcionario> nome(String nome) {
+		return (root, CriteriaQuery, CriteriaBuilder) -> 
+		CriteriaBuilder.like(root.get("nome"), "%" + nome + "%");
+	}
+	public static Specification<Funcionario> cpf(String cpf) {
+		return (root, CriteriaQuery, CriteriaBuilder) -> 
+		CriteriaBuilder.equal(root.get("cpf"),cpf);
+	}
+	public static Specification<Funcionario> salario(Double salario) {
+		return (root, CriteriaQuery, CriteriaBuilder) -> 
+		CriteriaBuilder.greaterThan(root.get("salario"), salario);
+	}
+	public static Specification<Funcionario> dataContratacao(LocalDate dataContratacao) {
+		return (root, CriteriaQuery, CriteriaBuilder) -> 
+		CriteriaBuilder.greaterThan(root.get("data"), dataContratacao);
+	}
+	
+	
+}
